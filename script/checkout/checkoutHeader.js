f9045e3bda2c2316341792ffa7823b80a18430a0
MalikJi <parv240385@gmail.com>
2026-01-14 10:37:14 +0530

    Add jasmin testing framework

--- script/checkout/checkoutHeader.js
+++ script/checkout/checkoutHeader.js
@@ -3,7 +3,7 @@ import {totalCartItems} from '../../data/cart.js';
 export function checkoutHeaderLoad(){
     const totalItems = totalCartItems();
     const headerHtml = `Checkout (<a class="return-to-home-link js-top-cart-items-count"
-        href="amazon.html">${totalItems}</a>)`;
+        href="amazon.html">${totalItems} items</a>)`;
 
     document.querySelector('.js-checkout-header').innerHTML = headerHtml;
 }
