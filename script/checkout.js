5050570a2408da58c4cd41e9f846af8bea1f09b2
MalikJi <parv240385@gmail.com>
2026-01-18 23:47:55 +0530

    Load Products from the backend

--- script/checkout.js
+++ script/checkout.js
@@ -1,9 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+import {loadProducts} from "../data/products.js";
 // import '../data/cart-opp.js';
 // import '../data/cart-class.js';
 // import '../data/car.js';
-import '../data/backendPractice.js';
+// import '../data/backendPractice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(()=> {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
